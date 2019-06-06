5bc695450887ef08ec80faccda4afb1dec34d1fe
charlychiu <charlychiu@gmail.com>
2019-06-06 20:16:51 +0800

    Update utils

--- utils.py
+++ utils.py
@@ -1,7 +1,5 @@
-import pickle
-
-
 def save_variable_to_pickle(variable, name, path='./'):
+    import pickle
     if name is not None:
         file = open(path + name + '.pickle', 'wb')
         pickle.dump(variable, file)
@@ -9,3 +7,11 @@ def save_variable_to_pickle(variable, name, path='./'):
         print('Save to {} successfully'.format(str(name + '.pickle')))
     else:
         print('Error arg')
+
+def save_npy_array_to_picture(np_array, path='./data/'):
+    from PIL import Image
+    for idx, each_picture in enumerate(np_array):
+        img = Image.fromarray(each_picture)
+        img.mode = 'I'
+        img.save(path + str(idx) + '.png')
+        
